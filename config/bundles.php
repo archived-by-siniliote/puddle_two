b9d4c358b1f90a6749903114e2455a0b178e129a
sferey <stephane.ferey@gmail.com>
2022-06-06 20:07:01 +0200

    Revert "feat(dev): Install zenstruck/foundry"
    This reverts commit f1340b130f078e4b9d5ab4d840bcdc3d5500e0ed.

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
