263c241d505f5ee5099d717207feb7fd9116f44f
Stéphane Ferey <stephane.ferey@groupepsih.com>
2022-07-16 18:18:30 +0200

    feat(messenger): Ajout de symfony/messenger avec Redis

--- .mk/12_yarn.mk
+++ .mk/12_yarn.mk
@@ -9,11 +9,3 @@ yarn.install: ## Yarn: Install all dependencies.
 yarn.upgrade: ## Yarn: Upgrade packages to their latest version based on the specified range.
 	@echo -e "\033[1;43mYarn: Upgrade\033[0m"
 	$(YARN) upgrade
-
-.PHONY: node.sh
-node.sh: ## Yarn: App sh access by yarn container
-	$(EXEC_APP_NODE) sh
-
-.PHONY: node.sh.force
-node.sh.force: ## Yarn: App sh access by node container not running
-	$(RUN) --rm $(SERVICE_NODE) sh 
--- .mk/35_tools.mk
+++ .mk/35_tools.mk
@@ -1,4 +1,4 @@
-## PHPUNIT
+## PHPCS
 
 .PHONY: phpcs
 phpcs: ## PHP-CS-Fixer: Tools homepage
@@ -11,3 +11,21 @@ phpcs.show: ## PHP-CS-Fixer: Run and show console
 .PHONY: phpcs.fix
 phpcs.fix: ## PHP-CS-Fixer: Run and fix
 	$(PHP_CS_FIXER) fix
+
+## SH
+
+.PHONY: node.sh
+node.sh: ## Node: App sh access by node container
+	$(EXEC_APP_NODE) sh
+
+.PHONY: node.sh.force
+node.sh.force: ## Node: App sh access by node container not running
+	$(RUN) --rm $(SERVICE_NODE) sh
+
+.PHONY: php.sh
+php.sh: ## Php: App sh access by php container
+	$(EXEC_APP_PHP) sh
+
+.PHONY: php.sh.force
+php.sh.force: ## Node: App sh access by php container not running
+	$(RUN) --rm $(SERVICE_PHP) sh
